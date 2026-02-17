09d6de8f87dd13299188e152c43ae292f3af8cae
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-02-17 19:51:41 -0300

    df

--- hello.rb
+++ hello.rb
@@ -3,18 +3,25 @@ class NoteMap
   attr_reader :map, :reverse_map
 
   BASE_NOTES = {
-    "c" => 0, "cis" => 1, "des" => 1, "d" => 2, "dis" => 3, "ees" => 3,
-    "e" => 4, "f" => 5, "fis" => 6, "ges" => 6, "g" => 7, "gis" => 8,
-    "as" => 8, "a" => 9, "ais" => 10, "bes" => 10, "b" => 11
+    "g" => 0, "gis" => 1,
+    "as" => 1, "a" => 2, "ais" => 3, "bes" => 3, "b" => 4,
+    "c'" => 5, "des'" => 6, "cis'" => 6, "d'" => 7, "dis'" => 8, "ees'" => 8,
+    "e'" => 9, "f'" => 10, "ges'" => 11, "fis'" => 11
   }
 
   OCTAVES = ["", "'", "''", "'''", "''''", "'''''", "''''''"]
 
   INTERVAL_RULES = {
-    5 => 23,  # perfect fourth → +2 octaves
+
+    5 => 24,  # perfect fourth → +2 octaves
     7 => 19,  # perfect fifth → +major tenth
-    3 => 32,  # minor third → +3octaves - 1 tone
-    4 => 28   # major third → +2 octaves + M3
+    4 => 28,   # major third → +2 octaves + M3
+    3 => 32  # minor third → +3octaves - 1 tone
+
+
+
+
+
   }
 
   def initialize
@@ -25,8 +32,9 @@ class NoteMap
 
 
     add_chord_harmonics
-    add_artificial_harmonics
     add_natural_harmonics
+    add_artificial_harmonics
+
     build_reverse_map
 
 
@@ -47,20 +55,34 @@ class NoteMap
     ["g'", "d''", "a''", "e'''"].each do |open|
       @map["#{open}2^\\flageolet"] = @map[open]
     end
+
+
+
+
+
   end
 
   # --- Artificial harmonics ---
   def add_artificial_harmonics
-    @map["d''8\harmonic"] = @map["g'''"]
+    @map["d''8\harmonic"] = @map["a'''"]
     @map["e''8\harmonic"] = @map["e'''"]
     @map["c'8\\harmonic"] = @map["g''"]
     @map["d'8\\harmonic"] = @map["d''"]
     @map["a'8\\harmonic"] = @map["a''"]
-    @map["g'8\\harmonic"] = @map["g'''"] #4 stringt
-    @map["a'8\\harmonic"] = @map["a''"]
+    @map["g'8\\harmonic"] = @map["d'''"] #4 stringt
     @map["b''8\\harmonic"] = @map["b'''"] #4 stringt
     @map["a''8\\harmonic"] = @map["e''''"]
 
+    @map["e'8^\\harmonic"] = @map["b''"]
+    @map["b'8^\\harmonic"] = @map["fis''"]
+
+    @map["fis''8^\\harmonic"] = @map["des'''"]
+    @map["fis''8^\\harmonic"] = @map["cis'''"]
+    @map["ges''8^\\harmonic"] = @map["des'''"]
+    @map["ges''8^\\harmonic"] = @map["cis'''"]
+
+
+    @map["cis'''8^\\harmonic"] = @map["gis''''"]
   end
 
   # --- Chord harmonics ---
@@ -78,21 +100,24 @@ class NoteMap
     touched_notes = fundamentals.dup
   
     fundamentals.each do |fund|
-      touched_notes.each do |touch|
-        f_val = @map[fund]
-        t_val = @map[touch]
-        next unless f_val && t_val
-  
-        interval = t_val - f_val
-        if INTERVAL_RULES[interval]
-          sounding_val = f_val + INTERVAL_RULES[interval]
+      INTERVAL_RULES.keys.reverse.each do |myinterval|
+        touched_notes.reverse.each do |touch|
 
-          sounding_note = @reverse_map[sounding_val]
-          token = "<#{fund} #{touch}\\harmonic>"
-          p "sounding val #{sounding_val} f_val #{f_val} t_val #{t_val} token #{token} => #{sounding_note}"
-          @map[token] = sounding_val
-          # Optional: print what was added
-          #puts "#{token} => #{sounding_note}"
+          f_val = @map[fund]
+          t_val = @map[touch]
+          next unless f_val && t_val
+  
+          interval = t_val - f_val
+          if INTERVAL_RULES[interval] and interval == myinterval
+            sounding_val = f_val + INTERVAL_RULES[interval]
+
+            sounding_note = @reverse_map[sounding_val]
+            token = "<#{fund} #{touch}\\harmonic>"
+            #p "sounding val #{sounding_val} f_val #{f_val} t_val #{t_val} token #{token} => #{sounding_note}"
+            @map[token] = sounding_val
+            # Optional: print what was added
+            #puts "#{token} => #{sounding_note}"
+          end
         end
       end
     end
@@ -138,8 +163,9 @@ class NoteMap
 
   # --- Scale generator ---
   def scale(start_note, intervals)
-    result = [start_note]
+
     current = @map[start_note]
+    result = [(@reverse_map[current] || current.to_s)]
 
     intervals.each do |step|
       current += step
@@ -149,7 +175,7 @@ class NoteMap
       result << (@reverse_map[current] || current.to_s)
     end
     #p result
-    result << "\\break"
+    result << " \\break "
 
     #p result
     result
@@ -164,8 +190,8 @@ chromatic_pattern = [1] * 12
 
 #puts "C major scale:"
 hello =""
-puts note_map.scale("c''", major_pattern).join(" ")
-hello << note_map.scale("c''", major_pattern).join(" ")
+puts note_map.scale("g''", major_pattern).join(" ")
+hello << note_map.scale("g''", major_pattern).join(" ")
 puts note_map.scale("d''", chromatic_pattern).join(" ")
 hello << note_map.scale("d''", chromatic_pattern).join(" ")
 
--- new_score.ly
+++ new_score.ly
@@ -13,14 +13,14 @@ global = {
 violin = \absolute  {
   \global
   % En avant la musique.
-  c'' d''2^\flageolet <a e'\harmonic> <bes f'\harmonic> c'8\harmonic a''2^\flageolet <e' b'\harmonic> <f' c''\harmonic> \breakd'' <as ees'\harmonic> <a e'\harmonic> <bes f'\harmonic> <b ges'\harmonic> c'8\harmonic <des' as'\harmonic> a''2^\flageolet <ees' bes'\harmonic> <e' b'\harmonic> <f' c''\harmonic> <ges' des''\harmonic> <g' d''\harmonic> \breakg'' <des' as'\harmonic> a''2^\flageolet <ees' bes'\harmonic> <e' b'\harmonic> <f' c''\harmonic> <ges' des''\harmonic> <g' d''\harmonic> <as' ees''\harmonic> e'''2^\flageolet <bes' f''\harmonic> <b' ges''\harmonic> g'8\harmonic \break
+  c'8\harmonic a'8\harmonic e'8^\harmonic <f' c''\harmonic> g'8\harmonic e''8harmonic <b' ges''\harmonic> <c'' g''\harmonic>  \break d'8\harmonic <as dis'\harmonic> <a e'\harmonic> <bes f'\harmonic> b'8^\harmonic c'8\harmonic <cis' gis'\harmonic> a'8\harmonic <ees' ais'\harmonic> e'8^\harmonic <f' c''\harmonic> ges''8^\harmonic g'8\harmonic  \break c'8\harmonic <cis' gis'\harmonic> a'8\harmonic <ees' ais'\harmonic> e'8^\harmonic <f' c''\harmonic> ges''8^\harmonic g'8\harmonic <as' dis''\harmonic> e''8harmonic <bes' f''\harmonic> <b' ges''\harmonic> <c'' g''\harmonic>  \break 
 }
 \score {
   \new Staff \with {
     instrumentName = "Violon"
     midiInstrument = "violin"
   } \violin
-  layout { }
+  \layout { }
   \midi {
     \tempo 4=100
   }
